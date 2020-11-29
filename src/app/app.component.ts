1cb1497f7f28244ce0a2065575a20fe888582e16
Casey <casey.gardonio@foat.org>
2020-11-29 15:53:46 -0600

    completed all three assignments for section 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  detailsVisible: boolean = false
-  buttonClicks = []
+  detailsVisible: boolean = false;
+  buttonClicks= [];
+  totalClicks: number = 0;
 
   onButtonClick() {
     this.detailsVisible = !this.detailsVisible;
-
-    let totalClicks: number = 0;
-    totalClicks = totalClicks+1
-    console.log(totalClicks);
-    this.buttonClicks.push(totalClicks);
+    this.totalClicks += 1;
+    // this.buttonClicks.push(this.totalClicks);
+    //alternative to creating variable "totalClicks" to hold inscrementing number and push to array is:
+    //this.buttonClicks.push(this.buttonClicks.length + 1);
+    this.buttonClicks.push(new Date());
+    //Date() is a built-in JS method that generates a timestamp
   }
 
   // getColor() {
-  //   if(this.instance>4){
-  //     return 'blue'
-  //   };
+  //   if(this.totalClicks>4) {
+  //     return 'blue' 
+  //   }
   // }
 }
